2db2af6a76f6836ff72678ba213401cdb328815a
Illia-Illia <Takoexerrorx@gmail.com>
2022-12-07 09:50:47 +0200

    min style

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <ChakraProvider>
             <App />
           </ChakraProvider>
@@ -21,5 +21,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
-
-// basename = '/goit-react-hw-08-phonebook/';
